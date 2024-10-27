0c09cf3fa29a295b226ac702831bbfe86885f0f8
Volha Yakauleva <yvvolha@gmail.com>
2024-10-27 23:32:06 +0200

    feat: add info messages helper

---
+++ src/ws_server/utils/get-info-messages.ts
@@ -0,0 +1,3 @@
+export const getWsServerStartedMessage = (port: number) => `Started WS server on localhost:${port}!`;
+
+export const getHttpServerStartedMessage =(port: number) => `Started static HTTP server on localhost:${port}!`;
